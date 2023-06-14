3b13ef7af67eae8deadb3aada816903c188a928a
Victor Colovatto Borioli <borioli_12@hotmail.com>
2023-06-14 19:43:38 -0300

    Adicionar arquivos de projeto.

---
+++ mvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()        
+        {
+            return View();           
+        }
+
+        public IActionResult Welcome(string nome = "Carlos", int numTimes = 1)
+        {
+            ViewData["Message"] = nome;
+            ViewData["NumTimes"] = 10;
+           
+            return View();
+        }
+    }
+}
